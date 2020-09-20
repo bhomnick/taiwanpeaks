189c23c43ecb84631fe40b2e181a5eba13030a73
Ben Homnick <bhomnick@gmail.com>
2020-09-20 16:46:06 +0800

    move manage up, updates to s3 seetings

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
+
+
+sys.path.insert(0, str(Path('.') / 'taiwanpeaks'))
 
 
 def main():
--- taiwanpeaks/taiwanpeaks/settings.py
+++ taiwanpeaks/taiwanpeaks/settings.py
@@ -24,8 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'herokuapp',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -43,7 +42,7 @@ ROOT_URLCONF = 'taiwanpeaks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,42 +102,24 @@ USE_TZ = True
 # Static files
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
-STATICFILES_STORAGE = 'require_s3.storage.OptimizedCachedStaticFilesStorage'
-
-
-# Storage
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # AWS
 
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default='')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default='')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default='')
-AWS_AUTO_CREATE_BUCKET = True
-AWS_HEADERS = {
-    'Cache-Control': 'public, max-age=86400',
-}
-AWS_S3_FILE_OVERWRITE = False
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_SECURE_URLS = True
-AWS_REDUCED_REDUNDANCY = False
-AWS_IS_GZIPPED = False
+USE_S3 = env.bool('USE_S3', default=False)
 
+if USE_S3:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default='')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default='')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default='')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-ap-northeast-1.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_LOCATION = 'static'
 
-# Cache
-
-"""
-CACHES = {
-    'staticfiles': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'TIMEOUT': 60 * 60 * 24 * 365,
-        'LOCATION': 'staticfiles',
-    },
-}
-"""
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Logging
