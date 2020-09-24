0b2699d8bf13f894df6b7c8ae74315d0387fc2b5
Ben Homnick <bhomnick@gmail.com>
2020-09-24 22:50:21 +0800

    added route detail

--- taiwanpeaks/routes/urls.py
+++ taiwanpeaks/routes/urls.py
@@ -4,5 +4,6 @@ from routes import views
 
 
 urlpatterns = [
-    path('', views.routes_list, name='routes-list')
+    path('', views.routes_list, name='routes-list'),
+    path('detail', views.routes_detail, name='routes-detail')
 ]
--- taiwanpeaks/routes/views.py
+++ taiwanpeaks/routes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def routes_list(request):
     return render(request, 'routes/list.html')
+
+
+def routes_detail(request):
+    return render(request, 'routes/detail.html')
