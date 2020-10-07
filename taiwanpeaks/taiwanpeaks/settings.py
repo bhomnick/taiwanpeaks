151933da6b4f29b82cfeb24582958bf714bed517
Ben Homnick <bhomnick@gmail.com>
2020-10-07 22:55:47 +0800

    update db settings

--- taiwanpeaks/taiwanpeaks/settings.py
+++ taiwanpeaks/taiwanpeaks/settings.py
@@ -95,16 +95,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'taiwanpeaks.wsgi.application'
 
 
-# Database
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
