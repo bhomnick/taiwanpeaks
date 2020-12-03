a336133ef8eb7642c7f227d0e84487e1e74a548f
Ben Homnick <bhomnick@gmail.com>
2020-12-03 17:01:04 +0800

    hide inactive routes

--- taiwanpeaks/routes/views.py
+++ taiwanpeaks/routes/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 
@@ -14,6 +15,9 @@ def routes_list(request):
 
 def routes_detail(request, slug):
     route = get_object_or_404(Route, slug=slug)
+    if not route.active and not request.user.is_staff:
+        raise Http404
+
     return render(request, 'routes/detail.html', {
         'route': route,
         'map_api_key': settings.THUNDERFOREST_API_KEY
